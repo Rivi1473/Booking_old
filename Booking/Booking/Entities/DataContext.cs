0bbb715eb578828f848236489c9a91cdfe3b708f
Rivi1473 <rivi1473@gmail.com>
2023-11-08 18:27:02 +0200

    DataContext

---
+++ Booking/Booking/Entities/DataContext.cs
@@ -0,0 +1,21 @@
+﻿namespace Booking.Entities
+{
+    public class DataContext
+    {
+        public List<Order> OrdersList { get; set; }
+        public List<Zimmer> ZimmersList { get; set; }
+        public List<Renter> RentersList { get; set; }
+        public int CntOrder { get; set; }
+        public int CntZimmer { get; set; }
+        public int CntRenter { get; set; }
+        public DataContext()
+        {
+            OrdersList = new List<Order>();
+            ZimmersList = new List<Zimmer>();
+            RentersList = new List<Renter>();
+            CntOrder = 1;
+            CntZimmer = 1;
+            CntRenter = 1;
+        }
+    }
+}
